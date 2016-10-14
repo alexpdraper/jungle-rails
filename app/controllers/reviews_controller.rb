0748dbaa05d9d257f855027afe21e6c31ac72a83
Alex <alexpdraper@gmail.com>
2016-10-14 23:35:26 +0000

    Added login authentication for adding reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,11 @@
 class ReviewsController < ApplicationController
 
+  before_filter :require_login
+
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
-    @review[:user_id] = current_user.id
+    @review.user = current_user
 
     if @review.save
       redirect_to product_path(@product)
@@ -15,6 +17,13 @@ class ReviewsController < ApplicationController
   private
     def review_params
       params.require(:review).permit(:rating, :description)
+    end
+
+    def require_login
+      unless current_user
+        flash[:error] = 'You must be logged in to review products'
+        redirect_to login_path
+      end
     end
 
 end
