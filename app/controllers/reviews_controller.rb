4affa0b9016ef4364832f2f38ec89bfe32485c61
Alex <alexpdraper@gmail.com>
2016-10-15 01:37:21 +0000

    Can delete reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,6 +14,15 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+
+    @review.destroy if current_user == @review.user
+
+    redirect_to product_path(@product)
+  end
+
   private
     def review_params
       params.require(:review).permit(:rating, :description)
