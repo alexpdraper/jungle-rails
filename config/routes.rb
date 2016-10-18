4f366c5768b01cb78d9dc5813ff83d4770c28ed1
Alex <alexpdraper@gmail.com>
2016-10-18 01:44:58 +0000

    Refactor

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  # resources :sessions, only: [:new, :create, :destroy]
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
